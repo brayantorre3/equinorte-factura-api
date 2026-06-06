d132962996c8295a6f4bd160a4ceeec6840fc3c3
BrayanT99 <brayan201115@gmail.com>
2026-06-05 21:29:53 -0500

    feat: agregar FacturaRepository con JpaRepository

---
+++ src/main/java/factura_api/repository/FacturaRepository.java
@@ -0,0 +1,9 @@
+package factura_api.repository;
+
+import factura_api.model.Factura;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FacturaRepository extends JpaRepository<Factura, Long> {
+}
