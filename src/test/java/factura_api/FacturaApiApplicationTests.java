6f9d0312a2a9698d0ef9062b8f174d8eaff291e6
BrayanT99 <brayan201115@gmail.com>
2026-06-05 21:33:18 -0500

    chore: agregar archivos de configuracion del proyecto

---
+++ src/test/java/factura_api/FacturaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package factura_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
